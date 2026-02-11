9afc4f4bbdf9d344c24aeb1e502a9f167e755a7f
juhreisss <juhreis2508@gmail.com>
2026-02-11 11:06:41 -0300

    Primeiro projeto

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TelaInicial());
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
