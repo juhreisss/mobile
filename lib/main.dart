7c14a77882355b52bcf53a0a46c508d5e21af6d6
juhreisss <juhreis2508@gmail.com>
2026-02-18 11:05:54 -0300

    18/02

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,37 @@ class TelaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("App1")),
+        body: Column(
+          spacing: 20,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(width: 100, height: 100, color: Colors.blue),
+            Container(width: 100, height: 100, color: Colors.red),
+            Container(width: 100, height: 100, color: Colors.green),
+            Row(
+              spacing: 20,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 100, height: 100, color: Colors.yellow),
+                Container(width: 100, height: 100, color: Colors.purple),
+                Container(width: 100, height: 100, color: Colors.orange),
+              ],
+            ),
+            SizedBox(height: 20),
+            Stack(
+              children: [
+                Container(width: 100, height: 100, color: Colors.black),
+                Container(width: 50, height: 50, color: Colors.blue),
+                Container(width: 25, height: 25, color: Colors.yellow),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
